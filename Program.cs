5ecb9302a39fe1735778fa9d1108d7d25b5690a1
Bruno Brasolin <brunobrasolinc@gmail.com>
2024-02-17 00:06:49 -0300

    Ordem dos tratores

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Gamidas.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.SetBasePath("/app/config");
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,8 +14,6 @@ builder.Services.AddDbContext<MainDatabaseContext>(option => option.UseOracle(bu
 builder.Services.AddHealthChecks().AddOracle(builder.Configuration.GetConnectionString("Default"));
 builder.Services.AddCors();
 builder.Services.ConfigureGamidas();
-builder.Configuration.SetBasePath("/app/config");
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 var app = builder.Build();
 
