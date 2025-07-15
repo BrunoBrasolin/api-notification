c957571ce147447ec0038141461b0a54f8578dee
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2025-07-14 23:50:40 -0300

    Teste URL

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -6,27 +6,31 @@ using Microsoft.AspNetCore.Mvc;
 namespace api_notification.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("")]
 public class NotificationController(IMediator mediator) : Controller
 {
-    private readonly IMediator _mediator = mediator;
+	private readonly IMediator _mediator = mediator;
 
-	[HttpGet("List")]
-    public async Task<List<NotificationModel>> List([FromQuery] ListRequest request) => await _mediator.Send(request);
+	[HttpGet]
+	public async Task<List<NotificationModel>> List([FromQuery] ListRequest request) => await _mediator.Send(request);
 
-    [HttpGet("Get")]
-    public async Task<NotificationModel> Get([FromQuery] GetRequest request) => await _mediator.Send(request);
+	[HttpGet("{id}")]
+	public async Task<NotificationModel> Get(int id) => await _mediator.Send(new GetRequest { Id = id });
 
-    [HttpPost("Create")]
-    public async Task<bool> Create([FromBody] CreateRequest request) => await _mediator.Send(request);
+	[HttpPost]
+	public async Task<bool> Create([FromBody] CreateRequest request) => await _mediator.Send(request);
 
-    [HttpPut("Update")]
-    public async Task<bool> Update([FromBody] UpdateRequest request) => await _mediator.Send(request);
+	[HttpPut("{id}")]
+	public async Task<bool> Update(int id, [FromBody] UpdateRequest request)
+	{
+		request.Notification.Id = id;
+		return await _mediator.Send(request);
+	}
 
-    [HttpDelete("Delete")]
-    public async Task<bool> Delete([FromBody] DeleteRequest request) => await _mediator.Send(request);
+	[HttpDelete("{id}")]
+	public async Task<bool> Delete(int id) => await _mediator.Send(new DeleteRequest { Id = id });
 
-    [HttpGet("Dismiss")]
-    public async Task<bool> Dismiss([FromQuery] DismissRequest request) => await _mediator.Send(request);
+	[HttpGet("Dismiss")]
+	public async Task<bool> Dismiss([FromQuery] DismissRequest request) => await _mediator.Send(request);
 
 }
